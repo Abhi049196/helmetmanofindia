aea73fc22e727023458ae2f1ec434fb95194a073
premmshanddilya <rockingprem2252@gmail.com>
2025-12-26 12:56:40 +0530

    Initial commit

---
+++ js/footer-script.js
@@ -0,0 +1,190 @@
+// Define the HTML string
+const footerHTML = `
+<footer class="main-footer">
+    <div class="auto-container">
+    <footer class="main-footer">
+    <div class="auto-container">
+        <!--Upper Box-->
+        <div class="upper-box">
+            <div class="row clearfix">
+                <!--Footer Logo-->
+                <div class="logo-column col-md-6 col-sm-6 col-xs-12">
+                    <div class="logo">
+                        <a href="#"><img src="images/helmetman-logo-circle.png" alt="" /></a>
+                    </div>
+                </div>
+                <!--Nav Column-->
+                <div class="nav-column col-md-6 col-sm-6 col-xs-12">
+                    <div class="inner-column">
+                        <ul class="footer-nav">
+                            <li><a href="privacy.html">Privacy & Policy</a></li>
+                            <li><a href="term.html">Terms & Condition</a></li>
+                            <li><a href="legal.html">Legal</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Widgets Section-->
+        <div class="widgets-section">
+            <div class="row clearfix">
+
+                <!--Network Column-->
+                <div class="column network-column col-md-5 col-sm-6 col-xs-12">
+                    <!--Network Info Tabs-->
+                    <div class="network-info-tabs">
+                        <!--Network Tabs-->
+                        <div class="network-tabs tabs-box">
+
+                            <!--Tab Btns-->
+                            <ul class="tab-btns tab-buttons clearfix">
+                                <li class="our-branches dropdown"><a class="btn btn-default dropdown-toggle"
+                                        id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="true" href="#"><span
+                                            class="icon flaticon-international"></span> <span
+                                            class="toggle-text">India</span> <span
+                                            class="arrow fa fa-caret-down"></span></a>
+                                </li>
+                            </ul>
+
+                            <!--Tabs Container-->
+                            <div class="tabs-content">
+
+                                <!--Tab / Active Tab-->
+                                <div class="tab active-tab" id="network-one">
+                                    <div class="content">
+                                        <div class="row clearfix">
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text">HELMET MAN FOUNDATION B-4/14C,
+                                                    KESHAVPURAM,DELHI-110035</div>
+                                            </div>
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text"> <a href="tel:9155793183"> Ph: +91 9155793183</a>
+                                                    <br><a
+                                                        href="mailto:Info@helmetmanofindia.com">E:Info@helmetmanofindia.com</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="working-text"><span>Working hrs:</span> Mon - Satday: 9am to
+                                            5pm.</div>
+                                    </div>
+                                </div>
+
+                                <!-- Tab -->
+                                <div class="tab" id="network-two">
+                                    <div class="content">
+                                        <div class="row clearfix">
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text">198 Tech Road Bridge Str, Newyork 10026 United
+                                                    States.</div>
+                                            </div>
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text">Ph: 936.452.8005 <br> E: Mailus@ikonteam.com</div>
+                                            </div>
+                                        </div>
+                                        <div class="working-text"><span>Working hrs:</span> Mon - Satday: 9am to
+                                            5pm.</div>
+                                    </div>
+                                </div>
+
+                                <!-- Tab -->
+                                <div class="tab" id="network-three">
+                                    <div class="content">
+                                        <div class="row clearfix">
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text">Tech Road Bridge Str, Newyork 10026 United States.
+                                                </div>
+                                            </div>
+                                            <div class="column col-md-6 col-sm-12 col-xs-12">
+                                                <div class="text">Ph: 936.452.8005 <br> E: Mailus@ikonteam.com</div>
+                                            </div>
+                                        </div>
+                                        <div class="working-text"><span>Working hrs:</span> Mon - Satday: 9am to
+                                            5pm.</div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Links Column-->
+                <div class="column links-column col-md-3 col-sm-6 col-xs-12">
+                    <ul class="links-footer">
+                        <li><a href="../about.html">About Us</a></li>
+                        <li><a href="../blog.html">Blog</a></li>
+                        <li><a href="../gallery.html">Gallery</a></li>
+                        <li><a href="../contact.html">Contact Us</a></li>
+                        <li><a href="../media_coverage/award.html">Awards</a></li>
+                    </ul>
+                </div>
+
+                <!--Appointment Column-->
+                <div class="column appointment-column col-md-4 col-sm-6 col-xs-12">
+                    <div class="inner-column">
+                        <h2>Need Help of Our Experts? <br> Make an Appoinemnt.</h2>
+                        <!--Phone Number-->
+                        <div class="phone-form">
+                            <form method="post" action="newsletter.php">
+                                <div class="form-group clearfix">
+                                    <input type="tel" name="number" value="" placeholder="Your Ph Num..." required>
+                                    <button type="submit" class="theme-btn subscribe-btn">Ok</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--Footer Bottom-->
+    <div class="footer-bottom">
+        <div class="auto-container">
+            <div class="row clearfix">
+
+                <!--Column-->
+                <div class="column col-md-6 col-sm-12 col-xs-12 text-center">
+                    <div class="copyright">Copyright &copy; 2023 <a href="index.html">Helmet Man Of India</a> All
+                        rights reserved. <br>Designed by <a
+                            href="https://www.linkedin.com/in/abhishek-kumar-649356211/" target="_blank">Mr. Avi
+                            Sharma</a>
+                    </div>
+                </div>
+
+                <!--Social Column-->
+                <div class="social-column col-md-6 col-sm-12 col-xs-12">
+                    <ul class="social-icon-one">
+                        <li><a href="https://www.facebook.com/Helmetmanindia/"><span
+                                    class="fa fa-facebook"></span></a></li>
+                        <li><a href="https://twitter.com/helmet_man_?t=lH1k8xItzMCJJfXz7K4odA&s=09"><span
+                                    class="fa fa-twitter"></span></a></li>
+                        <li><a
+                                href="https://in.linkedin.com/in/helmet-man-of-india-366b9152?trk=public_post_feed-actor-name"><span
+                                    class="fa fa-linkedin"></span></a></li>
+                        <li><a href="https://www.instagram.com/helmet_man_of_india/?hl=en"><span
+                                    class="fa fa-instagram"></span></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
+    </div>
+</footer>
+`;
+
+// Create a div element
+const footerContainer = document.createElement('div');
+
+// Assign the HTML string to the div's innerHTML property
+footerContainer.innerHTML = footerHTML;
+
+// Append the div to the document body
+document.body.appendChild(footerContainer);
